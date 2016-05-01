ea6e8bbc473d90b7ebb4fa74c8184bd3ec43a17d
neo <15634211355@163.com>
2016-05-01 11:39:13 +0800

    功能完善

--- js/main.js
+++ js/main.js
@@ -3,9 +3,64 @@
  */
 $(function(){
    (function(window,document,$){
-       //设置进度条和按钮的拖拽
+       var media=$('#media')[0];
+       //设置进度条和按钮
        dragX($('.pro-btn')[0],0,480);
        dragY($('.vol-btn')[0],0,80);
+       $('.vol').click(function(){
+          $('.vol-control').toggle();
+       });
+       $('.vol-control').bind('click', function () {
+           return false;         //阻止冒泡
+       });
+
+       //设置歌曲列表的显示和隐藏
+       $('.up-down').bind('click', function(){
+           if($('.song-list').is(':visible')){
+               $('.song-list').slideUp(500);
+               $(this).css({
+                   '-webkit-transform':'rotate3d(1,0,0,0deg)',
+                   '-moz-transform'   :'rotate3d(1,0,0,0deg)',
+                   '-ms-transform '   :'rotate3d(1,0,0,0deg)',
+                   '-o-transform  '   :'rotate3d(1,0,0,0deg)',
+                   'transform     '   :'rotate3d(1,0,0,0deg)',
+                   'top':'0'
+               })
+           }else{
+               $('.song-list').slideDown(500);
+               $(this).css({
+                   '-webkit-transform':'rotate3d(1,0,0,180deg)',
+                   '-moz-transform'   :'rotate3d(1,0,0,180deg)',
+                   '-ms-transform '   :'rotate3d(1,0,0,180deg)',
+                   '-o-transform  '   :'rotate3d(1,0,0,180deg)',
+                   'transform     '   :'rotate3d(1,0,0,180deg)',
+                   'top':'43rem'
+               })
+           }
+       });
+
+
+       //绑定播放按钮
+       $('.play-btn').bind('click',function(){
+           playAudio();
+       });
+       function playAudio() {
+           if(media.paused) {
+               play();
+           } else {
+               pause();
+           }
+       }
+       //播放
+       function play() {
+           media.play();
+
+       }
+       //暂停
+       function pause() {
+           media.pause();
+          
+       }
 
 
 
@@ -14,5 +69,5 @@ $(function(){
 
 
 
-})(window,document,jQuery);
+   })(window,document,jQuery);
 });
