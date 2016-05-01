606bd2687d7fce1433d6fdbae073c6f9640b086f
neo <15634211355@163.com>
2016-05-01 10:22:51 +0800

    a music player

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 4/28/2016.
+ */
+$(function(){
+   (function(window,document,$){
+       //设置进度条和按钮的拖拽
+       dragX($('.pro-btn')[0],0,480);
+       dragY($('.vol-btn')[0],0,80);
+
+
+
+
+
+
+
+
+})(window,document,jQuery);
+});
