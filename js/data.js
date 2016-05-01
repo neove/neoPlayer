3c43cff5bb6ba9978d2dd92c85036fb113583cc8
neo <15634211355@163.com>
2016-05-01 22:17:26 +0800

    完善

--- js/data.js
+++ js/data.js
@@ -6,36 +6,42 @@ var data=[
         'name':'See You Again',
         'singer':'Charlie Puth',
         'src':'music/SeeYouAgain.mp3',
-        'songbg':'img/songbg1.png'
+        'songbg':'img/songbg1.png',
+        'lyric':"[00:11.62]It's Been A Long Day [00:15.62]Without You My Friend[00:18.42]And I'll Tell You All[00:19.88]About It When I See You Again[00:24.61]We've Come A Long Way[00:28.11]From Where We Began[00:30.46]OH I'll Tell You All About[00:32.82]It When I See You Again[00:36.72]When I See You Again[00:41.99]Why do you have to leave so soon[00:44.55]Why do you have to go[00:47.50]Why do you have to leave me[00:49.40]when I needed you the most[00:53.61]Cause I don't really know[00:55.26]how to tell ya without[00:57.26]feeling much worse[01:00.07]I know you're in a better place[01:01.97]and it's always gonna hurt[01:04.73]Carry on give me all[01:09.33]the strength I need[01:11.03]Carry on[01:17.18]It's Been A Long Day[01:21.08]Without You My Friend[01:24.02]And I'll Tell You All About[01:25.72]It When I See You Again[01:30.07]We've Come A Long Way[01:33.57]From Where We Began[01:35.98]OH I'll Tell You All About[01:38.58]It When I See You Again[01:42.13]When I See You Again[01:59.60]How do I breath without you[02:02.35]Feeling so cold[02:05.35]I'll be waiting right here[02:07.20]for ya till the day you're home[02:10.77]Carry on give me all[02:14.62]the strength I need[02:16.47]To carry on[02:22.40]So let the light guide your way[02:30.35]hold every memory as you go[02:35.62]And every road you take will[02:39.67]always lead you home[02:47.58]It's Been A Long Day[02:51.58]Without You My Friend[02:54.28]And I'll Tell You All About[02:56.08]It When I See You Again[03:00.34]We've Come A Long Way[03:03.80]From Where We Began[03:06.20]OH I'll Tell You All About It[03:09.40]When I See You Again[03:12.55]When I See You Again"
     },
     {
         'name':'Vagrant',
         'singer':'Veela Fenit',
         'src':'music/Vagrant.mp3',
-        'songbg':'img/songbg2.png'
+        'songbg':'img/songbg2.png',
+        'lyric':"[00:22.89]There's a reason she's alone[00:26.25]You can't pin her down[00:28.20]Because no one needs her home[00:31.53]Or needs her around, she says[00:34.18]There's beauty in the hills[00:37.18]A chip in the sky[00:39.18]So don't be sad because I roam[00:42.09]It keeps me alive.[00:47.83]To impress her[00:49.69]It's too damn hard[00:53.26]To stay loyal without going too far[00:58.84]If I see her again promise[01:01.24]I won't[01:02.09]Cry[01:31.77]To impress her[01:33.82]It's too damn hard[01:37.18]To stay loyal without going too far[01:42.72]If I see her again promise[01:45.27]I won't[01:46.27]Cry[01:52.42]She wasn't back tonight[01:55.26]And I don't know about you[01:57.76]My money's on the night[02:00.46]It takes her away like I do[02:03.26]My charm is twinkling out of her eyes[02:09.86]Of her eyes She will never grow old[02:16.20]Or change her mind[02:17.75]She'll talk about it later on[02:21.20]From seeing sights[02:23.20]There's something wonderful and odd[02:26.71]About the escape[02:28.81]While you want them to be gone[02:32.07]You want them to stay[02:37.58]To impress her it's just so damn hard[02:43.11]I want her back,[02:44.51]Please don't go too far[02:48.52]If I see her again promise[02:50.97]I won't[02:52.38]Cry[03:21.45]To impress her is just too damn hard[03:26.87]And I want her back[03:28.37]But she's gone too far[03:32.37]If I see her again, promise I won't[03:35.87]Cry?"
     },
     {
         'name':'Oceanside',
         'singer':'Lainey Lou',
         'src':'music/Oceanside.mp3',
-        'songbg':'img/songbg3.png'
+        'songbg':'img/songbg3.png',
+        'lyric':"[00:24.56]prince charming sing to me[00:27.51]all that I'm missing[00:30.36]wish you could see the smile[00:34.36]I'm trying to with hold[00:36.61]you're leaving little behind[00:39.82]except this memory of time[00:42.53]all the sun can never[00:45.65]shine as bright as you[01:00.51]you leave for one moment[01:03.47]everyone can't see it[01:06.47]nothing is breaking[01:08.94]and god's grace is what I'm living[01:12.52]together stars will fall[01:15.47]the satellite hit the walls[01:18.47]along with every word[01:22.27]that I am singing[01:25.98]so now I'm writing a song[01:29.04]praying you shall sing along[01:32.75]to it some day[01:37.99]this maybe haven't done well[01:41.00]a lot is as far as I can tell[01:45.11]I'm in harmony[01:49.00]and I am putting one foot[01:52.24]out that door[02:05.28]I know it's silly to say[02:08.18]just how the simple days[02:10.96]keep messing with my plan[02:17.07]a thousand dimond rings[02:20.03]though nothing I can see[02:23.85]can even comprehend[02:28.62]your eyes meaning thousand days ahead[02:32.17]so now I'm writing a song[02:34.97]praying you shall sing along[02:38.87]to it some day[02:44.33]this maybe haven't done well[02:47.13]a lot is as far as I can tell[02:51.34]I'm in harmony[02:54.86]and I am putting one foot[02:58.04]out that door[03:09.54]so now I'm writing a song[03:12.49]praying you shall sing along[03:16.33]to it some day[03:21.52]this maybe haven't done well[03:24.47]a lot is as far as I can tell[03:28.73]I'm in harmony[03:32.34]and the oceanside is what[03:35.34]I'm searching for[03:50.05]prince charming sing to me[03:52.86]all that I am missing[03:55.86]all the sun can never shine[03:59.97]as bright as you"
     },
     {
         'name':'Nothing but a Heart beat',
         'singer':'Say Lou Lou',
         'src':'music/NothingbutaHeartbeat.mp3',
-        'songbg':'img/songbg4.png'
+        'songbg':'img/songbg4.png',
+        'lyric':""
     },
     {
         'name':'Let Go',
         'singer':'Jon McLaughlin',
         'src':'music/LetGo.mp3',
-        'songbg':'img/songbg5.png'
+        'songbg':'img/songbg5.png',
+        'lyric':""
     },
     {
         'name':'Everything We Touch',
         'singer':'Say Lou Lou',
         'src':'music/EverythingWeTouch.mp3',
-        'songbg':'img/songbg6.png'
+        'songbg':'img/songbg6.png',
+        'lyric':""
     }
 ]
